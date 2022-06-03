d00c6ccabe7e9f25cf750e7ce545e12b68a4969d
Rodan Sotto <rodan.sotto@outlook.com>
2022-06-02 20:04:05 -0400

    owin pipeline mvc 5 demo code

---
+++ App_Start/Startup.MyOwinComponents.cs
@@ -0,0 +1,160 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyMvc5App
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public partial class Startup
+    {
+        public void WriteHelloWorld(IAppBuilder app)
+        {
+            app.Run(ctx =>
+            {
+                return ctx.Response.WriteAsync("From WriteHelloWorld: Hello World!!!");
+            });
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            await _next(environment);
+
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                writer.Write("From HelloWorldComponent: Hello World!!!");
+            }
+        }
+    }
+
+    public class HelloWorldStartEndComponent
+    {
+        AppFunc _next;
+
+        // through this constructor, the app.Use<T>() passes the next component that this component will call
+        public HelloWorldStartEndComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        // an OWIN component needs to provide a method with a Func<IDictionary<string, object>, Task> signature
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            // make sure this middleware component only executes when request path is /HelloWorldOWINPipeline
+            // but it will still need to call the next component
+            var path = environment["owin.RequestPath"] as string;
+            if (path.Equals("/HelloWorldOWINPipeline", StringComparison.OrdinalIgnoreCase))
+            {
+                // create a new environment variable to hold the hello world string in multiple languages
+                environment["helloworld"] = "<div class='container body-content'><h1>";
+
+                // call the next component in the OWIN pipeline
+                await _next(environment);
+
+                // at this point all the hello world components in the pipeline should have inserted
+                //  their hello world string in their own language to the hello world environment variable,
+                //  so output the resulting string to response
+                // but first let's put the closing h1 and div tags at the end
+                environment["helloworld"] = environment["helloworld"] + "</h1></div>";
+
+                var response = environment["owin.ResponseBody"] as Stream;
+                var helloWorld = environment["helloworld"] as string;
+                using (var writer = new StreamWriter(response))
+                {
+                    writer.Write(helloWorld);
+                }
+            }
+            else
+            {
+                // call the next component in the OWIN pipeline
+                await _next(environment);
+            }
+        }
+    }
+
+    public class HelloWorldEnglishComponent
+    {
+        AppFunc _next;
+        public HelloWorldEnglishComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            // make sure this middleware component only executes when request path is /HelloWorldOWINPipeline
+            var path = environment["owin.RequestPath"] as string;
+            if (path.Equals("/HelloWorldOWINPipeline", StringComparison.OrdinalIgnoreCase))
+            {
+                environment["helloworld"] = environment["helloworld"] + "Hello World!!!<br/>";
+            }
+
+            await _next(environment);
+        }
+    }
+
+    public class HelloWorldFrenchComponent
+    {
+        AppFunc _next;
+        public HelloWorldFrenchComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            // make sure this middleware component only executes when request path is /HelloWorldOWINPipeline
+            var path = environment["owin.RequestPath"] as string;
+            if (path.Equals("/HelloWorldOWINPipeline", StringComparison.OrdinalIgnoreCase))
+            {
+                environment["helloworld"] = environment["helloworld"] + "Bonjour Tout Le Monde!!!<br/>";
+            }
+
+            await _next(environment);
+        }
+    }
+
+    public class HelloWorldTagalogComponent
+    {
+        AppFunc _next;
+        public HelloWorldTagalogComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            // make sure this middleware component only executes when request path is /HelloWorldOWINPipeline
+            var path = environment["owin.RequestPath"] as string;
+            if (path.Equals("/HelloWorldOWINPipeline", StringComparison.OrdinalIgnoreCase))
+            {
+                environment["helloworld"] = environment["helloworld"] + "Mabuhay!!!<br/>";
+            }
+
+            await _next(environment);
+        }
+
+    }
+
+    public static class MyAppBuilderExtensions
+    {
+        public static void UseHelloWorldFrenchComponent(this IAppBuilder app)
+        {
+            app.Use<HelloWorldFrenchComponent>();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartupAttribute(typeof(MyMvc5App.Startup))]
+namespace MyMvc5App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // this is the OWIN middleware component for individual authentication added by Visual Studio
+            //ConfigureAuth(app);
+
+            // this is an extension method from the nuget package Microsoft.Owin.Diagnostics
+            // a wrapper to app.Use<T>()
+            // this does not pass control to next component in the pipeline
+            //app.UseWelcomePage();
+
+            // this is my custom function, a wrapper to app.Run() 
+            //  and app.Run() does not pass control to next component in the pipeline
+            //WriteHelloWorld(app);
+
+            // calling app.Use<T>() to register my OWIN middleware component in the OWIN pipeline
+            //app.Use<HelloWorldComponent>();
+
+            // Basically you call app.Use<T>(), or an AppBuilder extension which is just a wrapper to app.Use<T>(), 
+            //  or app.Run() to register an OWIN middleware component in the OWIN pipeline
+
+            // Below is an example of my Hello World!!! OWIN pipeline
+            // Basically each component adds a hello world string in their own language to the
+            //  OWIN environment variable, and once done it outputs the resulting string to response
+            // These components awaits on the next component in the pipeline so essentially everything
+            //  will come back to the first component in the pipeline and my first component in the
+            //  pipeline is HelloWorldStartEndComponent and this component is the one that outputs
+            //  the resulting hello world string to response
+
+            // this is the first middleware component in the pipeline
+            // it's calling app.Use<T>() to register the component in the OWIN pipeline
+            app.Use<HelloWorldStartEndComponent>();
+
+            // this is the second middleware component in the pipeline
+            app.Use<HelloWorldEnglishComponent>();
+
+            // this is the third middleware component in the pipeline
+            //app.Use<HelloWorldFrenchComponent>();
+            // instead, it calls a custom extension method to AppBuilder that wraps app.Use<T>()
+            app.UseHelloWorldFrenchComponent();
+
+            // and this is the last middleware component in the pipeline
+            //app.Use<HelloWorldTagalogComponent>();
+            // you can also pass a lambda function in calls to app.Use() instead
+            app.Use(async (ctx, next) =>
+            {
+                // make sure this middleware only executes when request path is HelloWorldOWINPipeline
+                var path = ctx.Environment["owin.RequestPath"] as string;
+                if (path.Equals("/HelloWorldOWINPipeline", StringComparison.OrdinalIgnoreCase))
+                {
+                    ctx.Environment["helloworld"] = ctx.Environment["helloworld"] + " Mabuhay!!! ";
+                }
+
+                await next();
+            });
+
+            //app.MapSignalR();
+        }
+    }
+}
